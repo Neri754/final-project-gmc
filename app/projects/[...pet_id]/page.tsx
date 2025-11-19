ca5bc5af4ab10b590c5626ada1a86d4ee90fbc59
neri754 <celestineogar13@gmail.com>
2025-11-19 06:38:56 -0800

    fix errors

--- app/projects/[...pet_id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter, useParams } from "next/navigation";
-import { projects, IProject } from "@/data";
-import { useEffect, useState } from "react";
-
-const Pages = () => {
-  return <div></div>;
-};
-
-export default Pages;
